1a673a0c14850410cf19303fd04098f223740ee7
Hungry Man <katsu.matsushita@gmail.com>
2021-08-02 04:05:09 -0400

    Changed the API call to be a 6 day forecast. This will provide today's forecase and 5 future days.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,41 @@ var currentHumidity = document.querySelector("cityHumidity");
 var currentUV = document.querySelector("cityUV");
 
 function startSearch() {
+    // the search button got clicked, and everything starts here
     citySearchTxt = document.querySelector("#searchText").value;
     console.log(citySearchTxt);
-    console.log(moment().format("MM/DD/YYYY"));
+    currentDay = moment().format("MM/DD/YYYY");
     if (citySearchTxt !== null) {
         // check that the city is a valid city by sending the query to the API
-
+        cityName = citySearchTxt;
+        makeQuery();
+        getWeatherData();
+        
     }
+}
+
+function makeQuery() {
+    // creates a query URL for the city. returns today and 5-day forecast
+    queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&cnt=6&appid=" + apiKey;
+}
+
+function getWeatherData() {
+    // gets the weather data from the API
+    fetch(queryURL)
+        .then(response => {
+            // check to see that the response is ok, then return it as a json object
+            if(response.ok){
+                return response.json();
+            } else {
+                // else there's something wrong with the query, send up a modal
+                console.log(response.status)
+                throw response.json();
+            }
+        })
+        .then(data => {
+            // the weather data has been returned, this is where the functions to display the data get called
+            console.log(data);
+        })
 }
 
 searchBtn.addEventListener("click", startSearch);
